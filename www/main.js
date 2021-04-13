ebc9e43bad03c11f278f5b139a5b358e796b38ba
elpbatista <elpbatista@gmail.com>
2021-04-13 13:58:33 -0700

    more fixes

--- www/main.js
+++ www/main.js
@@ -2,39 +2,18 @@
 version 2
 */
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWxwYmF0aXN0YSIsImEiOiJ4YlBmNmlJIn0.x0lZKcK907BWTS4LV9dkHA';
+
 const SAERI = 'https://ims.saeri.org/fias-1956-thumbnail/';
-var sources = {
-  imagery: {
-    'type': 'geojson',
-    'data': '../data/1956-aerial-imagery.geojson'
-  },
-  flightLines: {
-    type: 'geojson',
-    data: '../data/1956-flight-lines.geojson'
+
+var images = $.ajax({
+  url: "../data/process/1956-aerial-imagery.geojson",
+  dataType: "json",
+  // success: console.log("Data successfully loaded."),
+  error: function(xhr) {
+    alert(xhr.statusText)
   }
-}
-var layers = {
-  flightLines: {
-    id: 'flightLines',
-    type: 'line',
-    source: sources.flightLines,
-    paint: {
-      'line-color': '#486DE0',
-    },
-    layout: {
-      // 'visibility': 'none'
-    }
-  },
-  squares: {
-    id: 'squares',
-    type: 'line',
-    source: sources.imagery,
-    paint: {
-      'line-color': ['match', ['get', 'present'], 0, '#cd0000', '#8dad24'],
-      'line-opacity': .65,
-    }
-  }
-};
+});
+
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/satellite-v9?optimize=true',
@@ -43,35 +22,67 @@ var map = new mapboxgl.Map({
   hash: true
 });
 map.on('load', function() {
-  map.addLayer(layers.flightLines);
-  map.addLayer(layers.squares);
-  map.addControl(new mapboxgl.ScaleControl());
-  var images = $.ajax({
-    url: "../data/process/1956-aerial-imagery.geojson",
-    dataType: "json",
-    success: console.log("Data successfully loaded."),
-    error: function(xhr) {
-      alert(xhr.statusText)
+  map.addSource('imagery', {
+    type: 'geojson',
+    data: '../data/1956-aerial-imagery.geojson '
+  });
+  map.addLayer({
+    id: 'flightLines',
+    type: 'line',
+    minzoom: 9,
+    maxzoom: 13,
+    source: {
+      type: 'geojson',
+      data: '../data/1956-flight-lines.geojson'
+    },
+    paint: {
+      'line-color': '#486DE0',
+    },
+    layout: {
+      // 'visibility': 'none'
     }
   });
+  map.addLayer({
+    id: 'squares',
+    type: 'line',
+    source: 'imagery',
+    minzoom: 9,
+    maxzoom: 13,
+    paint: {
+      'line-color': ['match', ['get', 'present'], 0, '#cd0000', '#8dad24'],
+      'line-opacity': .65,
+    }
+  });
+  map.addLayer({
+    id: 'geotiff',
+    type: 'fill',
+    source: 'imagery',
+    maxzoom: 10,
+    paint: {
+      'fill-color': ['match', ['get', 'geotiff'], 1, '#ff7f00', 'transparent'],
+      'fill-opacity': .75,
+    }
+  });
+  map.addControl(new mapboxgl.ScaleControl());
   $.when(images).done(function() {
-    images.responseJSON.features.forEach(function(currentFeature) {
-      if (currentFeature.properties.geotiff == 1) {
-        polyPhoto = currentFeature.geometry.coordinates[0];
-        imageName = currentFeature.properties.name;
-        map.addLayer({
-          id: imageName,
-          type: 'raster',
-          source: {
-            type: 'image',
-            url: '../img/' + imageName + '.jpg',
-            coordinates: [polyPhoto[2], polyPhoto[1], polyPhoto[0], polyPhoto[3]]
-          },
-          paint: {
-            'raster-opacity': 0.75
-          },
-        });
-      };
+    const geotiffs = images.responseJSON.features.filter(geotiff => geotiff.properties.geotiff == 1);
+    geotiffs.forEach(function(currentFeature) {
+      polyPhoto = currentFeature.geometry.coordinates[0];
+      imageName = currentFeature.properties.name;
+      map.addLayer({
+        id: imageName,
+        beforeId: 'squares',
+        type: 'raster',
+        // minzoom: 9,
+        source: {
+          type: 'image',
+          url: '../img/' + imageName + '.jpg',
+          coordinates: [polyPhoto[2], polyPhoto[1], polyPhoto[0], polyPhoto[3]]
+        },
+        paint: {
+          'raster-opacity': 0.65
+        },
+      });
     });
   });
 });
